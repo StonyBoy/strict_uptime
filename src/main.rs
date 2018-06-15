c7ae0ee36597b2b37404d557b7650f0e98b634f6
Steen Hegelund <steen.hegelund@microsemi.com>
2018-06-15 16:09:28 +0200

    First working version

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::Read;
+
+fn read_uptime() -> Result<u64, String> {
+    let mut file = match File::open("/proc/uptime") {
+        Ok(file) => file,
+        Err(err) => return Err(err.to_string()),
+    };
+    let mut contents = String::new();
+    if let Err(err) = file.read_to_string(&mut contents) {
+        return Err(err.to_string());
+    };
+    let value: u64 = match contents.split(".").next() {
+        Some(text) => match text.parse() {
+                Ok(value) => value,
+                Err(err) => return Err(err.to_string()),
+           }
+        None => return Err("No uptime value".to_string())
+    };
+    Ok(value)
+}
+
+fn strict_uptime(secs : u64) -> String {
+    const SECS_PER_DAY : u64 = 86400;
+    const SECS_PER_HOUR : u64 = 3600;
+    const SECS_PER_MINUTE : u64 = 60;
+    let days = secs / SECS_PER_DAY;
+    let remainder = secs % SECS_PER_DAY;
+    let hours = remainder / SECS_PER_HOUR;
+    let minutes = remainder % SECS_PER_HOUR / SECS_PER_MINUTE;
+    format!("{:>3} {:>02} {:>02}", days, hours, minutes)
+}
+
+fn main() {
+    match read_uptime() {
+        Ok(value) => println!("up(DHM): {}", strict_uptime(value)),
+        Err(err) => eprintln!("uptime: error {}", err.to_string()),
+    }
+}
